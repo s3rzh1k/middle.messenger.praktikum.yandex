ce1101b8c2d9ba05a117deacc0b1cb3a6f544e80
Sergey Babenyshev <sirGoonya@yandex.ru>
2023-03-21 23:17:55 +0500

    check netlify deploy

--- src/index.js
+++ src/index.js
@@ -14,33 +14,33 @@ switch (location) {
       comments: [{ subject: "subject", body: "body" }],
     });
     break;
-  case "login":
-    root.innerHTML = login({
-      comments: [{ subject: "subject", body: "body" }],
-    });
-    break;
-  case "register":
-    root.innerHTML = register({
-      comments: [{ subject: "subject", body: "body" }],
-    });
-    break;
-  case "broken":
-    root.innerHTML = broken({
-      comments: [{ subject: "subject", body: "body" }],
-    });
-    break;
-  case "profile":
-    root.innerHTML = profile({
-      email: "pochta@yandex.ru",
-      login: "ivanivanov",
-      first_name: "Иван",
-      second_name: "Иванов",
-      display_name: "Иван",
-      phone: "+7 (909) 967 30 30",
-    });
-    break;
-  default:
-    root.innerHTML = notFound({
-      comments: [{ subject: "subject", body: "body" }],
-    });
+//   case "login":
+//     root.innerHTML = login({
+//       comments: [{ subject: "subject", body: "body" }],
+//     });
+//     break;
+//   case "register":
+//     root.innerHTML = register({
+//       comments: [{ subject: "subject", body: "body" }],
+//     });
+//     break;
+//   // case "broken":
+//   //   root.innerHTML = broken({
+//   //     comments: [{ subject: "subject", body: "body" }],
+//   //   });
+//   //   break;
+//   case "profile":
+//     root.innerHTML = profile({
+//       email: "pochta@yandex.ru",
+//       login: "ivanivanov",
+//       first_name: "Иван",
+//       second_name: "Иванов",
+//       display_name: "Иван",
+//       phone: "+7 (909) 967 30 30",
+//     });
+//     break;
+//   default:
+//     root.innerHTML = notFound({
+//       comments: [{ subject: "subject", body: "body" }],
+//     });
 }
--- src/pages/broken/broken.js
+++ src/pages/broken/broken.js
@@ -1,4 +1,7 @@
-import broken from './broken.hbs';
+import brokenTemplate from './broken.hbs';
 import './broken.scss';
 import key from '../../../static/images/key.svg';
-export default (props) => broken({...props, key});
+export const broken = (props) => brokenTemplate({...props, key});
+
+const root = document.querySelector("#root");
+root.innerHTML = broken();
